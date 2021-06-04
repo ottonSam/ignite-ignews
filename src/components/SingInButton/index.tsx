8d7aa879825eba8ee8adb02309bd2f3ab5d1fcdf
ottonsam <ottonsam@gmail.com>
2021-06-04 15:20:42 -0300

    Create singinbutton component

---
+++ src/components/SingInButton/index.tsx
@@ -0,0 +1,27 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+
+import styles from './styles.module.scss';
+
+export function SingInButton() {
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button 
+      type="button"
+      className={styles.singInButton}
+    >
+      <FaGithub color="#04d361"/>
+      Samuel Ótton
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button 
+      type="button"
+      className={styles.singInButton}
+    >
+      <FaGithub color="#eba417"/>
+      singIn with GitHub
+    </button>
+  )
+}
