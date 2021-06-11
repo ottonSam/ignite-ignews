fdc15f255aa7db06433a824747755b2bf0eb0577
ottonsam <ottonsam@gmail.com>
2021-06-11 15:05:43 -0300

    Exib posts on posts page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,24 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
 import { getPrismicClient } from '../../services/prismic';
 
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -15,21 +27,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Desenvolvimento de Jogos Digitais: do storytelling à programação</strong>
-            <p>Os jogos digitais já estão presentes no nosso dia a dia e muitas vezes não percebemos o quanto. A Pesquisa Game Brasil 2021 mostra que aproximadamente 72% dos brasileiros jogam games. São os filhos que passam horas jogando on-line, ou somos nós mesmos que usamos nosso tempo livre para se entreter em jogos nos nossos smartphones, até mesmo em propagandas e anúncios eles estão lá, capturando nossa atenção.</p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Desenvolvimento de Jogos Digitais: do storytelling à programação</strong>
-            <p>Os jogos digitais já estão presentes no nosso dia a dia e muitas vezes não percebemos o quanto. A Pesquisa Game Brasil 2021 mostra que aproximadamente 72% dos brasileiros jogam games. São os filhos que passam horas jogando on-line, ou somos nós mesmos que usamos nosso tempo livre para se entreter em jogos nos nossos smartphones, até mesmo em propagandas e anúncios eles estão lá, capturando nossa atenção.</p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Desenvolvimento de Jogos Digitais: do storytelling à programação</strong>
-            <p>Os jogos digitais já estão presentes no nosso dia a dia e muitas vezes não percebemos o quanto. A Pesquisa Game Brasil 2021 mostra que aproximadamente 72% dos brasileiros jogam games. São os filhos que passam horas jogando on-line, ou somos nós mesmos que usamos nosso tempo livre para se entreter em jogos nos nossos smartphones, até mesmo em propagandas e anúncios eles estão lá, capturando nossa atenção.</p>
-          </a>
+          { posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -46,9 +50,22 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100,
   });
 
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  });
+
   console.log(response);
 
   return {
-    props: {}
+    props: {posts}
   }
 }
