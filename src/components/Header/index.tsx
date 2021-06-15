94c27c15e8a4600a132b9fa184d2418ab164b687
ottonsam <ottonsam@gmail.com>
2021-06-14 23:25:18 -0300

    Create activelink component for use automatic style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,22 @@
-import Link from 'next/link';
 import { SingInButton } from '../SingInButton';
+
+import { ActiveLink } from '../ActiveLink';
 
 import styles from './styles.module.scss';
 
 export function Header() {
+
   return(
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <Link href="/">
-            <a className={styles.active}>Home</a>
-          </Link>
-          <Link href="/posts" prefetch>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
             <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
 
         <SingInButton />
